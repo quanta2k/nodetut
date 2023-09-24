d74138d522788450896bec8215c6d5a239fb1ee3
quanta2k <72522871+quanta2k@users.noreply.github.com>
2023-09-24 23:46:15 +0530

    Add .gitignore, 12-http.js, update app.js, package-lock.json, package.json

---
+++ 12-http.js
@@ -0,0 +1,18 @@
+const { log } = require('console');
+const http=require('http');
+
+const server = http.createServer((req,res)=>{
+    if(req.url==='/'){
+        res.end('welcome to our homepage')
+    }
+    if(req.url==='about'){
+        res.end('Here is our short history')
+    }
+    res.end(`
+    <h1>Oops!</h1>
+    <p>We cant seem to find the page you are looking </p>
+    <a href="/" back home</a>`)
+
+})
+
+server.listen(5000)
